22cb12199bd857975843712b6a59f9f0a882a026
Jaroslav Týc <mail@jaroslavtyc.com>
2016-09-30 22:14:32 +0200

    Fixed tests for PHP 7+

--- Doctrineum/Tests/tests_bootstrap.php
+++ Doctrineum/Tests/tests_bootstrap.php
@@ -6,5 +6,5 @@ $loader->add('Doctrine\\Tests', __DIR__ . '/../../vendor/doctrine/dbal/tests');
 $loader->add('Doctrine\\Tests', __DIR__ . '/../../vendor/doctrine/orm/tests');
 
 error_reporting(-1);
-ini_set('display_errors', true);
-ini_set('xdebug.max_nesting_level', 100);
+ini_set('display_errors', '1');
+ini_set('xdebug.max_nesting_level', '100');
